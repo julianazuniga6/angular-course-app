c05fac6651bcc2414bb4926f0699ff31a7842931
Juliana Zuniga <julizuoso@gmail.com>
2022-01-14 18:32:46 -0500

    Add functionality to Add btn in shopping-edit using ViewChild

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Output, ViewChild, EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/shared/models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild("nameInput") nameInput: ElementRef;
+  @ViewChild("amountInput") amountInput: ElementRef;
+
+  @Output() addIngredientEvent = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddIngredient() {
+    const newIngredient = new Ingredient(
+      this.nameInput.nativeElement.value,
+      this.amountInput.nativeElement.value
+    );
+    this.addIngredientEvent.emit(newIngredient);
   }
 
 }
